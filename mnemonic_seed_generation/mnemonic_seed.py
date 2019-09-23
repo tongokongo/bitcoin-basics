7410c5be89898b796bb31c268bdb3c4844c2e47c
tongokongo <radmanmen@gmail.com>
2019-09-23 05:57:40 -0700

    recorded video

--- mnemonic_seed_generation/mnemonic_seed.py
+++ mnemonic_seed_generation/mnemonic_seed.py
@@ -1,51 +1,49 @@
 import os
 import binascii
 import hashlib
+import unicodedata
 
 bits = 256
-bytes = int(bits/8) #32 bytes
-random_256 = binascii.hexlify(os.urandom(bytes))
-bin_random_256 = binascii.unhexlify(random_256)
+print("Bytes = " + str(bits//8))
+#r = os.urandom(bits//8)
+random_bin = binascii.unhexlify("68a79eaca2324873eacc50cb9c6eca8cc68ea5d936f98787c60c7ebc74e6ce7c") #random in bin
+random_hex = binascii.hexlify(random_bin) #random in hex
+bytes = len(random_bin)
 
+hashed_sha256 = hashlib.sha256(random_bin).hexdigest()
+print("My sha256: " + str(hashed_sha256))
 
-print("My random hex " + str(bits) + "  bits number: " + random_256.decode("utf-8"))
-#how to print bin_random_256
+checksum = bits/32 #8 bits
 
-hashed_sha_256 = hashlib.sha256(random_256).hexdigest()
-print(hashed_sha_256)
-
-entropy = bits/32 #8 bits = 2hex characters
-
-
-my_hexdata = "ff"
-num_of_bits = 8
-print(bin(int(my_hexdata, 16))[2:].zfill(num_of_bits)) #16 = hexadecimal base
-
-
-
-b = (
-        bin(int(random_256, 16))[2:].zfill(bytes * 8)
-        + bin(int(hashed_sha_256, 16))[2:].zfill(256)[: bytes * 8 // 32]
+bin_result = (
+    bin(int(random_hex, 16))[2:].zfill(bytes * 8)
+    + bin(int(hashed_sha256, 16))[2:].zfill(256)[: bytes * 8 // 32]
 )
+print("Bin result: " + str(bin_result))
 
 index_list = []
 with open("english.txt", "r", encoding="utf-8") as f:
     for w in f.readlines():
         index_list.append(w.strip())
 
-
 wordlist = []
-print(str(len(b) / 11))
-for i in range(len(b) // 11):
-    print(b[i*11 : (i + 1)*11])
-    index = int(b[i * 11 : (i + 1) * 11], 2)
-    print(str(index))
+for i in range(len(bin_result) // 11):
+    #print(bin_result[i*11 : (i+1)*11])
+    index = int(bin_result[i*11 : (i+1)*11], 2)
+    #print(str(index))
     wordlist.append(index_list[index])
 
-print(wordlist)
-#test wordlist in https://iancoleman.io/bip39/
+phrase = " ".join(wordlist)
+print(phrase)
 
+#TO SEED
+normalized_mnemonic = unicodedata.normalize("NFKD", phrase)
+password = ""
+normalized_passphrase = unicodedata.normalize("NFKD", password)
 
+passphrase = "mnemonic" + normalized_passphrase
+mnemonic = normalized_mnemonic.encode("utf-8")
+passphrase = passphrase.encode("utf-8")
 
-
-
+bin_seed = hashlib.pbkdf2_hmac("sha512", mnemonic, passphrase, 2048)
+print(binascii.hexlify(bin_seed[:64]))
