876dbcbb641d69b76834583045ba8b54e18602ac
tongokongo <radmanmen@gmail.com>
2020-01-11 15:24:08 -0800

    recorded the video

--- base58/base58.py
+++ base58/base58.py
@@ -8,17 +8,16 @@ def b58encode(v):
         print("\nchar: {}".format(char))
         number += lev * char
         print("number: {}".format(number))
-        lev = lev << 8
+        lev = lev << 8 #2^8
         print("lev: {}".format(lev))
 
     string = ""
-    while number: #if number > 0...
+    while number: #if number > 0 then do X
         print("\n\n")
         number, modulo = divmod(number, 58)
         string = alphabet[modulo] + string
-        print("\n\n number: {} modulo: {} ".format(number, modulo))
+        print("\n number: {}, modulo: {}, string: {}".format(number, modulo, string))
     return string
-
 
 test = binascii.unhexlify("4d61")
 print(b58encode(test))
